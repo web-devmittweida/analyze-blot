a585b8142c4ad811f723d01912b565ac7ddc937b
web-devmittweida <73587667+web-devmittweida@users.noreply.github.com>
2020-12-06 23:33:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, make_response, request
+from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import json
+from capture import main
+from upload import get_frames
+import os
+
+cred = credentials.Certificate('hackprinceton.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/process/<interval>')
+def process(interval):
+    os.system("python3 capture.py" + " " + str(1000*int(interval)))
+    frame_analysis = get_frames(int(interval))
+    os.system("rm -rf *.png")
+    return jsonify(frame_analysis)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port = 80, debug = True)
---
+++ upload.py
@@ -0,0 +1,85 @@
+import json
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+from pydub import AudioSegment
+import pickle
+
+content = None
+frames = None
+# read frame data
+
+def process_clip():
+
+    client = speech.SpeechClient()
+
+    audio = types.RecognitionAudio(uri='gs://audio-hackprinceton19/video.wav')
+    config = types.RecognitionConfig(
+        encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz = 48000,
+        language_code='en-US',
+        enable_word_time_offsets = True)
+    response = client.long_running_recognize(config=config, audio=audio).result()
+    return response.results
+
+def get_frames(interval):
+    trans_results = process_clip()
+    content = []
+    for i in range(len(trans_results)):
+        content.append(trans_results[i].alternatives[0].words)
+    content = [(content[i][j].start_time.seconds, content[i][j].start_time.nanos, content[i][j].end_time.seconds,
+                content[i][j].end_time.nanos, content[i][j].word) for i in range(len(content)) for j in
+               range(len(content[i]))]
+
+    data = content
+    emotions = ("joy", "sorrow", "surprise", "anger")
+    with open("data.json", "r") as f:
+        frames = json.load(f)
+    for i, frame in enumerate(frames):
+        frames[i]["emotions"] = {}
+        for emotion in emotions:
+            frames[i]["emotions"][emotion] = frame[emotion]
+    frames = [{k: v for k, v in frames[i].items() if k not in emotions} for i in range(len(frames))]
+    start, finish = 0, interval
+    data_counter = 0
+    for i, frame in enumerate(frames):
+        text = ""
+        if data_counter >= len(data):
+            break
+        time = data[data_counter][2] + (data[data_counter][3] / (10**9))
+        while time <= finish and data_counter < len(data):
+            text += (data[data_counter][4] + " ")
+            time = data[data_counter][2] + (data[data_counter][3] / (10 ** 9))
+            data_counter += 1
+        start += interval
+        finish += interval
+        frames[i]["text"] = text
+    return frames
+
+
+def upload():
+
+    global content, frames
+
+    with open("data.json") as f:
+        frames = json.load(f)
+
+    audio_filepath = "./video.wav"
+    sound = AudioSegment.from_wav(audio_filepath)
+    sound = sound.set_channels(1)
+    sound.export(audio_filepath, format="wav")
+
+    trans_results = process_clip(audio_filepath)
+    content = []
+    for i in range(len(trans_results)):
+        content.append(trans_results[i].alternatives[0].words)
+    content = [(content[i][j].start_time.seconds, content[i][j].start_time.nanos, content[i][j].end_time.seconds, content[i][j].end_time.nanos, content[i][j].word) for i in range(len(content)) for j in range(len(content[i]))]
+
+    with open("test.pickle", "wb") as f:
+        pickle.dump(content, f)
+    with open("lmao.txt", "w") as file:
+        file.write(str(trans_results))
+
+if __name__  == '__main__':
+    #upload()
+    get_frames(5)
